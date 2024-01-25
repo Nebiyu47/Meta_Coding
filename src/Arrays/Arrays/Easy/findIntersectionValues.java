b0de19689cae5b26dfb5127dbe98f918855fce34
macpro <nebiyugirma7497@yahoo.com>
2024-01-25 13:58:17 -0800

    commit

--- src/Arrays/Arrays/Easy/findIntersectionValues.java
+++ src/Arrays/Arrays/Easy/findIntersectionValues.java
@@ -1,28 +1,51 @@
 package Arrays.Arrays.Easy;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class findIntersectionValues {
     public static void main(String[] args) {
 
     }
-    public static int [] findIntersectionValues(int [] nums1 , int[] nums2){
 
-        HashMap<Integer,Integer> map= new HashMap<>();
+    public static int[] findIntersectionValues(int[] nums1, int[] nums2) {
 
-        List<Integer> intersections= new ArrayList<>();
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
 
-        for(int n : nums1){
-            map.put(n ,map.getOrDefault(n,0)+1);
+        // Add elements of nums1 to set1
+        for (int num : nums1) {
+            set1.add(num);
         }
 
-        for(int num : nums2){
-            if(map.containsKey(num)&&map.get(num)>0){
-                intersections.add(num);
-                map.put(num)
+        // Calculate the count of common elements between nums1 and nums2
+        int commonCount = 0;
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                commonCount++;
             }
         }
+
+        // Add elements of nums2 to set2
+        for (int num : nums2) {
+            set2.add(num);
+        }
+
+        // Calculate the count of common elements between nums2 and nums1
+        int commonCount2 = 0;
+        for (int num : nums1) {
+            if (set2.contains(num)) {
+                commonCount2++;
+            }
+        }
+
+        // Create an ArrayList to store the counts
+        ArrayList<Integer> counts = new ArrayList<>();
+        counts.add(commonCount2);
+        counts.add(commonCount);
+
+        // Convert ArrayList to an array
+        int[] intersectionCounts = counts.stream().mapToInt(Integer::intValue).toArray();
+
+        return intersectionCounts;
     }
 }
