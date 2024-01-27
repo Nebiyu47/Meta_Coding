195dbcae63d0cbfb9a403f3b1a125d3df3b999df
macpro <nebiyugirma7497@yahoo.com>
2024-01-27 14:02:41 -0800

    9

--- src/Graphs/tree/SearchinaBinarySearchTree.java
+++ src/Graphs/tree/SearchinaBinarySearchTree.java
@@ -1,2 +1,20 @@
-package Graphs.tree;public class SearchinaBinarySearchTree {
+package Graphs.tree;
+
+public class SearchinaBinarySearchTree {
+    public static void main(String[] args) {
+
+    }
+    public static TreeNode searchBST(TreeNode root, int val){
+
+
+        if(root==null || root.val==val){
+            return root;
+        }
+        if(val<root.val){
+            return searchBST(root.left,val);
+        }else {
+            return searchBST(root.right,val);
+        }
+
+    }
 }
